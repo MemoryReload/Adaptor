26d188bbee5f37f419b90fb4787b52854648093a
heping <heping@xiavan.com>
2021-07-08 17:32:35 +0800

    the documentation for Adaptor.swift

--- Adaptor/Classes/Adaptor.swift
+++ Adaptor/Classes/Adaptor.swift
@@ -8,26 +8,36 @@
 import Foundation
 import UIKit
 
+
+
+/// The base protocol to define the most important elements to construct an adaptor.
 public protocol AdaptingProtocol {
     associatedtype V
     associatedtype D
     associatedtype C
     
+    /// The view  which adaptor is working for.
     var view: V? { get }
+    /// The extra context that adaptor needed to work properly with controllers and so on.
     var context: C { get set }
+    /// The display view model that adaptor needed to work with view.
     var dataSource:[D]? { get set }
 }
 
+/// The adaptor protocol that works with UITableView
 public protocol TableAdaptingProtocol: AdaptingProtocol, UITableViewDataSource, UITableViewDelegate { }
 
+/// The adaptor protocol that works with UICollectionView
 public protocol CollectionAdaptingProtocol: AdaptingProtocol, UICollectionViewDataSource, UICollectionViewDelegate { }
 
+/// The base adaptor that  works with UITableView
 public class TableAdaptor: NSObject, TableAdaptingProtocol {
     weak public fileprivate(set) var view: UITableView?
     public var context: AdaptorContextProtocol?
     public var dataSource:[TableSectionViewHolder]?
 }
 
+/// The base adaptor that works with UICollectionView
 public class CollectionAdaptor:NSObject, AdaptingProtocol {
     weak public fileprivate(set) var view: UICollectionView?
     public var context: AdaptorContextReusableProtocol?
@@ -37,9 +47,14 @@ public class CollectionAdaptor:NSObject, AdaptingProtocol {
 
 //MARK: AdaptorProtocol
 
+/// The protocol used to inject adaptor to the specified view
 public protocol AdaptorProtocol: AnyObject {
     associatedtype T
+    /// The adaptor for the specified view to work with
     var adaptor: T? { get }
+    
+    /// Connet the adaptor with the specified view
+    /// - Parameter adaptor: the adaptor to use
     func useAdaptor(_ adaptor: T?)
 }
 
@@ -84,20 +99,34 @@ extension UICollectionView: AdaptorProtocol {
 //MARK: View Register
 
 extension UITableView {
+    
+    /// regist table cell
+    /// - Parameter cellClass: the specified table cell class to regist
     public func registerCell(cellClass: UITableViewCell.Type){
         self.register(cellClass, forCellReuseIdentifier: NSStringFromClass(cellClass))
     }
     
+    
+    /// regist table section view
+    /// - Parameter viewClass: the specified table section class to regist
     public func registerSectionView(viewClass: UITableViewHeaderFooterView.Type){
         self.register(viewClass, forHeaderFooterViewReuseIdentifier: NSStringFromClass(viewClass))
     }
 }
 
 extension UICollectionView {
+    
+    /// regist collection cell
+    /// - Parameter cellClass: the specified collection cell class to regist
     public func registerCell(cellClass: UICollectionViewCell.Type){
         self.register(cellClass, forCellWithReuseIdentifier: NSStringFromClass(cellClass))
     }
     
+    
+    /// regist collection supplement view
+    /// - Parameters:
+    ///   - viewClass: the specified collection supplement view class to regist
+    ///   - kind: the specified collection supplement view kind
     public func regiserReusableView(viewClass: UICollectionReusableView.Type, forSupplementaryViewOfKind kind: String) {
         self.register(viewClass, forSupplementaryViewOfKind: kind, withReuseIdentifier: NSStringFromClass(viewClass))
     }
